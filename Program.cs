6cfdeffa3d7a6295302a83c689e3144a5f4180bb
rfranett <rfranett@gmail.com>
2023-11-30 22:20:21 -0300

    ifalinhados

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using project.net8.models ;
+
+int quantidadeEmEstoque = 3 ;
+int quantidadeDeCompra = 0 ;
+bool possivelVenda = quantidadeDeCompra > 0 && quantidadeEmEstoque >= quantidadeDeCompra;
+
+
+Console.WriteLine($"Quantidade Em Estoque:{quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade de Compra:{quantidadeDeCompra}");
+Console.WriteLine($"É Possivel Realizar a Venda? {possivelVenda}");
+
+if (quantidadeDeCompra == 0)
+{
+    Console.WriteLine("Venda Invalida");
+}
+else if (possivelVenda)
+{
+    Console.WriteLine("Venda Realizada.");
+}
+else 
+{
+    Console.WriteLine("Desculpa. Não Temos a Quantidade Desejada em Estoque.");
+}
